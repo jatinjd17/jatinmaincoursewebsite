693b819ea3509c94a5fbe6f456f804e5ebf835e7
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-05-26 17:20:27 +0530

    6th commit change route to linux api

--- next.config.js
+++ next.config.js
@@ -4,29 +4,6 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/user",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self' 'http://blog.logrocket.com'; image-src 'https://unsplash.com'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com'",
-          },
-        ],
-      },
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
