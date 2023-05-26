072fe403c7bdf5b9aa40e5c73e5ac35efb9b6f11
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-05-26 15:36:29 +0530

    5th commit corrected code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,32 @@
 import "@/styles/globals.css";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Context from "../context/context";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GoogleOAuthProvider clientId="577101913991-3lui3759un3sfba21h201r9g8ouf7qi0.apps.googleusercontent.com">
-        <Context>
-          <Component {...pageProps} />
-        </Context>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Context>
+            <Component {...pageProps} />
+          </Context>
+        </ThemeProvider>
       </GoogleOAuthProvider>
     </>
   );
